7aae18c28390fb2979ea2bf0fcfc52f7a7dfb37a
EatFan <eatfan0921@163.com>
2025-12-23 15:19:11 +0800

    docs(XssHttpServletRequestWrapper): 添加XSS请求包装器类的详细JavaDoc注释
    - 为构造函数添加了参数和异常说明的JavaDoc注释
    - 为getParameter方法添加了返回值和参数说明的JavaDoc注释
    - 为getParameterValues方法添加了返回值和参数说明的JavaDoc注释
    - 为getHeader方法添加了返回值和参数说明的JavaDoc注释
    - 为getInputStream方法添加了返回值和异常说明的JavaDoc注释
    - 为getReader方法添加了返回值和异常说明的JavaDoc注释
    - 为getContentLength方法添加了返回值说明的JavaDoc注释
    - 为getContentLengthLong方法添加了返回值说明的JavaDoc注释

--- junoyi-framework/junoyi-framework-web/src/main/java/com/junoyi/framework/web/xss/XssHttpServletRequestWrapper.java
+++ junoyi-framework/junoyi-framework-web/src/main/java/com/junoyi/framework/web/xss/XssHttpServletRequestWrapper.java
@@ -20,18 +20,36 @@ public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
 
     private byte[] body;
 
+    /**
+     * 构造函数，创建XSS防护的HTTP请求包装器
+     *
+     * @param request 原始HTTP请求对象
+     * @throws IOException 读取请求体时可能抛出的IO异常
+     */
     public XssHttpServletRequestWrapper(HttpServletRequest request) throws IOException {
         super(request);
         // 缓存请求体
         body = request.getInputStream().readAllBytes();
     }
 
+    /**
+     * 获取经过XSS过滤的请求参数值
+     *
+     * @param name 参数名称
+     * @return 经过XSS清理的参数值
+     */
     @Override
     public String getParameter(String name) {
         String value = super.getParameter(name);
         return XssUtils.clean(value);
     }
 
+    /**
+     * 获取经过XSS过滤的请求参数值数组
+     *
+     * @param name 参数名称
+     * @return 经过XSS清理的参数值数组
+     */
     @Override
     public String[] getParameterValues(String name) {
         String[] values = super.getParameterValues(name);
@@ -44,12 +62,24 @@ public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
         return cleanValues;
     }
 
+    /**
+     * 获取经过XSS过滤的请求头值
+     *
+     * @param name 请求头名称
+     * @return 经过XSS清理的请求头值
+     */
     @Override
     public String getHeader(String name) {
         String value = super.getHeader(name);
         return XssUtils.clean(value);
     }
 
+    /**
+     * 获取经过XSS过滤的输入流
+     *
+     * @return 经过XSS清理的Servlet输入流
+     * @throws IOException IO异常
+     */
     @Override
     public ServletInputStream getInputStream() throws IOException {
         // 过滤请求体中的 XSS
@@ -81,18 +111,35 @@ public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
         };
     }
 
+    /**
+     * 获取经过XSS过滤的字符读取器
+     *
+     * @return 经过XSS清理的字符读取器
+     * @throws IOException IO异常
+     */
     @Override
     public BufferedReader getReader() throws IOException {
         return new BufferedReader(new InputStreamReader(getInputStream(), StandardCharsets.UTF_8));
     }
 
+    /**
+     * 获取请求体内容长度
+     *
+     * @return 请求体内容长度
+     */
     @Override
     public int getContentLength() {
         return body.length;
     }
 
+    /**
+     * 获取请求体内容长度（长整型）
+     *
+     * @return 请求体内容长度
+     */
     @Override
     public long getContentLengthLong() {
         return body.length;
     }
 }
+
