49a2f461ea7ce0a4f26b6e9220ef74bdc1109d5e
EatFan <eatfan0921@163.com>
2025-12-24 17:11:31 +0800

    docs(SysMenuController): 添加系统菜单控制器类和方法注释
    - 为 SysMenuController 类添加类级别的 JavaDoc 注释
    - 为 getMenuList 方法添加方法级别的 JavaDoc 注释
    - 为 addMenu 方法添加方法级别的 JavaDoc 注释
    - 为 updateMenu 方法添加方法级别的 JavaDoc 注释
    - 完善了控制器的基本功能说明和参数返回值描述
    - 统一了代码注释格式和作者信息标注

--- junoyi-module/junoyi-module-system/src/main/java/com/junoyi/system/controller/SysMenuController.java
+++ junoyi-module/junoyi-module-system/src/main/java/com/junoyi/system/controller/SysMenuController.java
@@ -8,12 +8,23 @@ import com.junoyi.framework.security.enums.PlatformType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * 系统菜单控制器
+ * 提供菜单的增删改查等操作接口
+ *
+ * @author Fan
+ */
 @RestController
 @RequestMapping("/system/menu")
 @RequiredArgsConstructor
 public class SysMenuController extends BaseController {
 
-
+    /**
+     * 获取菜单列表
+     * 用于查询系统中所有的菜单信息
+     *
+     * @return R<?> 通用响应结果，包含菜单列表数据
+     */
     @GetMapping("/list")
     @PlatformScope(PlatformType.ADMIN_WEB)
     public R<?> getMenuList(){
@@ -21,13 +32,24 @@ public class SysMenuController extends BaseController {
         return R.ok();
     }
 
+    /**
+     * 添加菜单
+     * 用于向系统中新增菜单项
+     *
+     * @return R<?> 通用响应结果，表示添加操作的执行结果
+     */
     @PostMapping()
     @PlatformScope(PlatformType.ADMIN_WEB)
     public R<?> addMenu(){
         return R.ok();
     }
 
-
+    /**
+     * 更新菜单
+     * 用于修改系统中已存在的菜单信息
+     *
+     * @return R<?> 通用响应结果，表示更新操作的执行结果
+     */
     @PutMapping
     @PlatformScope(PlatformType.ADMIN_WEB)
     public R<?> updateMenu(){
