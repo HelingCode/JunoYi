cfa94dae46c3b2df267930b597cb003759697f7c
EatFan <eatfan0921@163.com>
2025-12-25 17:20:13 +0800

    feat(permission): 添加权限逻辑运算符和权限类型枚举
    - 新增 Logical 枚举类，定义逻辑与(AND)和逻辑或(OR)运算符
    - 删除 PermissionLevel 枚举类
    - 在 PermissionScope 注解中新增 PermissionType 类型字段
    - 新增 PermissionType 枚举类，定义API、菜单、按钮、组件、数据行、数据字段、数据操作等权限类型

--- junoyi-framework/junoyi-framework-permission/src/main/java/com/juynoyi/framework/permission/annotation/PermissionScope.java
+++ junoyi-framework/junoyi-framework-permission/src/main/java/com/juynoyi/framework/permission/annotation/PermissionScope.java
@@ -1,5 +1,7 @@
 package com.juynoyi.framework.permission.annotation;
 
+import com.juynoyi.framework.permission.enums.PermissionType;
+
 import java.lang.annotation.*;
 
 /**
@@ -11,5 +13,18 @@ import java.lang.annotation.*;
 @Target(ElementType.METHOD)
 @Documented
 public @interface PermissionScope {
+
+    /**
+     * 权限码（支持通配符）
+     * @return
+     */
     String[] value() default {};
+
+    /**
+     * 权限类型（默认API类型）
+     * @return
+     */
+    PermissionType type() default PermissionType.API;
+
+
 }
---
+++ junoyi-framework/junoyi-framework-permission/src/main/java/com/juynoyi/framework/permission/enums/PermissionType.java
@@ -0,0 +1,45 @@
+package com.juynoyi.framework.permission.enums;
+
+/**
+ * 权限等级枚举
+ *
+ * @author Fan
+ */
+public enum PermissionType {
+
+    /**
+     * API 接口
+     */
+    API,
+
+    /**
+     * 菜单访问
+     */
+    UI_MENU,
+
+    /**
+     * 按钮操作
+     */
+    UI_BUTTON,
+
+    /**
+     * 组件访问
+     */
+    UI_COMPONENT,
+
+    /**
+     * 数据行
+     */
+    DATA_ROW,
+
+    /**
+     * 数据字段
+     */
+    DATA_FIELD,
+
+    /**
+     * 数据操作
+     */
+    DATA_OPTIONS
+
+}
