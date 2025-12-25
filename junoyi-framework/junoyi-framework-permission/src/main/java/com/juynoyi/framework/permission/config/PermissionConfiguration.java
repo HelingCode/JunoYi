e32a6e3b58942556f8369e9b930e026d0ffdd6e1
EatFan <eatfan0921@163.com>
2025-12-25 12:56:08 +0800

    feat(permission): 添加权限框架基础配置
    - 创建了权限配置类 PermissionConfiguration
    - 新增权限等级枚举 PermissionLevel
    - 添加 SpringBoot 基础依赖
    - 集成 Spring 配置处理器依赖

---
+++ junoyi-framework/junoyi-framework-permission/src/main/java/com/juynoyi/framework/permission/config/PermissionConfiguration.java
@@ -0,0 +1,17 @@
+package com.juynoyi.framework.permission.config;
+
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * 权限配置类
+ * 用于定义和配置应用程序的权限相关设置，包括权限验证规则、访问控制策略等
+ * 该类作为Spring配置类，提供权限管理相关的Bean定义和配置
+ *
+ * @author Fan
+ */
+@Configuration
+public class PermissionConfiguration {
+
+
+}
---
+++ junoyi-framework/junoyi-framework-permission/src/main/java/com/juynoyi/framework/permission/enums/PermissionLevel.java
@@ -0,0 +1,11 @@
+package com.juynoyi.framework.permission.enums;
+
+/**
+ * 权限等级枚举
+ *
+ * @author Fan
+ */
+public enum PermissionLevel {
+
+
+}
