ace9fd4c292809fa4b88953c77bacc2e83344907
EatFan <eatfan0921@163.com>
2026-01-06 15:28:15 +0800

    feat(system): 添加权限池控制器
    - 创建了 SysPermissionPoolController 类
    - 配置了 /system/permission-pool 路由映射
    - 添加了 Lombok 注解支持
    - 实现了基础的控制器结构

---
+++ junoyi-module/junoyi-module-system/src/main/java/com/junoyi/system/controller/SysPermissionPoolController.java
@@ -0,0 +1,11 @@
+package com.junoyi.system.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/system/permission-pool")
+@RequiredArgsConstructor
+public class SysPermissionPoolController {
+}
